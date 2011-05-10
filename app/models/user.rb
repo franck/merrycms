1276cb70038c2445d491396a87eb66580b8ffe7e
Franck D'agostini <franck.dagostini@gmail.com>
2011-05-10 11:06:49 +0200

    adding defaut value for argument of last_admin?

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
     user_roles = roles.map{|r| r if self.has_role?(r.name) }
   end
   
-  def last_admin?(role)
+  def last_admin?(role=nil)
     last_admin = false
     if role == "admin" and self.has_role?("admin") and User.admins.size == 1
       last_admin = true
